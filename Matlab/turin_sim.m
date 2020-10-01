4f023fea948cfd0b5864a680675391c4a9b2e2dd
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2020-10-01 10:01:40 +0200

    Fix error in calculating channel response

--- Matlab/turin_sim.m
+++ Matlab/turin_sim.m
@@ -1,7 +1,7 @@
 %% clear
 clear 
 %%
-kmax = 1000; % Number of rays to include
+kmax = 100000; % Number of rays to include
 N = 100; % Number of data sets to generate
 
 % Generating phase phi as uniform r.v. between 0 and 2*pi
@@ -16,11 +16,11 @@ adist = makedist('Lognormal', mu, sigma);
 
 
 % t is a possion arrival process with mean delay lambda
-lambda = 10; % randomly chosen lambda
+lambda = 3; % randomly chosen lambda
 tdist = makedist('poisson',lambda);
 
 
-rho = zeros(kmax,N); % buffer for generated channel response data
+rho = zeros(100,N); % buffer for generated channel response data
 for n = 1:N
     phi = random(phidist,kmax,1); % Generate phase vector
     a = random(adist,kmax,1); % Generate amplitude vector
@@ -28,7 +28,7 @@ for n = 1:N
     for t = (1+min(tau)):(1+max(tau)) % For every time delay
         for k = 1:kmax % For every ray "k"
             if (t-1-tau(k) == 0) % if the time delay of k'th ray == current time delay
-                rho(t,n) = rho(t,n) + a(k) + exp(1j*phi(k)); 
+                rho(t,n) = rho(t,n) + a(k) * exp(1j*phi(k)); 
             end
         end
     end
@@ -36,8 +36,8 @@ end
 
 
 %% Compute ensemble mean of amplitude at delay t over N realisations
-rhomean = zeros(kmax,1); % buffer for ensemble mean of channel response
-for t=1:kmax %for every time delay 
+rhomean = zeros(100,1); % buffer for ensemble mean of channel response
+for t=1:length(rhomean) %for every time delay 
     for n = 1:N % for every experiment
         rhomean(t) = rhomean(t) + rho(t,n); % sum channel response
     end
@@ -46,10 +46,10 @@ end
         
 %% Plotting data
 
-trange = (0:kmax-1); % range of time delays  
+trange = (0:99); % range of time delays  
 
 plot(trange,abs(rhomean))
 title("Simulated response of radio channel using Turin model")
 xlabel("Time delay (arbitrary)")
 ylabel("Mean amplitude over " +  N + " samples with " + kmax + " rays")
-xlim([lambda-lambda*3/4 lambda+lambda*3/4])
+% xlim([lambda-lambda*3/4 lambda+lambda*3/4])

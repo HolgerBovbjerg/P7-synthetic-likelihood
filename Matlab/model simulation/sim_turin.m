ebf8ccdba80dcc1275e8909af9b16812e7c19abd
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2020-10-14 10:51:52 +0200

    Optimise code
    Change matlab "random" function call to distribution specific functions.
    Change noise addition and ifft to matrix operations.

--- Matlab/model simulation/sim_turin.m
+++ Matlab/model simulation/sim_turin.m
@@ -6,16 +6,11 @@ function [P_y, t] = sim_turin(N, B, Ns, T, G0, lambda, sigma_N)
     % We then simulate the transfer function, H_k, of the channel, using the
     % Turin model. 
 
-    Hk = zeros(Ns,N); % buffer for generated channel response data
-    P_h = zeros(Ns,N); % Buffer for Power of impulse response
-    ldist = makedist('poisson',tmax*lambda); % distribution for number 
-                                                 % of multipaths to include is 
-                                                 % a function of lambda and
-                                                 % maximum delay
+    Hk = zeros(Ns,N); % buffer for generated channel response data                                             
     % We run the simulation N times, creating new data sets for each
     % realization. 
     for n = 1:N
-        lmax = random(ldist,1,1);   % Number of multipath components, created from the Poisson distribution.
+        lmax = poissrnd(tmax*lambda);   % Number of multipath components, created from the Poisson distribution.
         tau = rand(lmax,1)*tmax;    % time-delays, drawn uniformly between 0 and the maximum delay.  
         tau = sort(tau);
         % For every multipath component a complex gain is generated, based on a
@@ -33,16 +28,15 @@ function [P_y, t] = sim_turin(N, B, Ns, T, G0, lambda, sigma_N)
                     Hk(k,n) = Hk(k,n) + alpha*exp(-1j*2*pi*deltaf*k*tau(i));
                 end
         end
-        %% Generate noise vector
-        noise = sigma_N^2 * (1/sqrt(2))* (randn(Ns,1) + 1j*randn(Ns,1));
-        %% Add noise to transfer function with complex normal dist
-        Hk(:,n) = Hk(:,n) + noise;
-        % Power delay profile
-        P_h(:,n) = abs(ifft(Hk(:,n))).^2;
     end
+    %% Generate noise vector
+    noise = sigma_N^2 * (1/sqrt(2))* (randn(Ns,N) + 1j*randn(Ns,N));
+    %% Add noise to transfer function with complex normal dist
+    Hk = Hk + noise;
+    %% Power delay profile
+    P_h = abs(ifft(Hk,[],1)).^2;
     %% Averaging over the N realizations
     P_h_mean = mean(P_h,2); % acg. power delay profile
-
     %% Simulating the power spectrum, P_h, from the transfer function
     % We use P_Y = E_s * P_h + noise (Noise is already included in simulation)
     P_y = B*P_h_mean; %+ sigma_N^2/Ns;

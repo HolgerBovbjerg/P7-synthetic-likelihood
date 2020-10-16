47ae2d7015fafd734e584a8b8a32ff3321df2244
clausmeyer <32387509+clausmeyer@users.noreply.github.com>
2020-10-16 12:25:06 +0200

    made bigplot better and BIGGER

--- Matlab/statistics/bigplot.m
+++ Matlab/statistics/bigplot.m
@@ -3,6 +3,7 @@
 tiledlayout(8,4)
 nexttile
 plot(Tvary,mean(temporal0T,2),"o")
+ylabel('Mean(m0)','FontSize',12)
 
 nexttile
 plot(lambdavary,mean(temporal0lambda,2),"o")
@@ -16,6 +17,7 @@ plot(G0vary,mean(temporal0G0,2),"o")
 
 nexttile
 plot(Tvary,mean(temporal1T,2),"o")
+ylabel('Mean(m1)','FontSize',12)
 
 nexttile
 plot(lambdavary,mean(temporal1lambda,2),"o")
@@ -28,6 +30,7 @@ plot(G0vary,mean(temporal1G0,2),"o")
 
 nexttile
 plot(Tvary,mean(temporal2T,2),"o")
+ylabel('Mean(m2)','FontSize',12)
 
 nexttile
 plot(lambdavary,mean(temporal2lambda,2),"o")
@@ -40,6 +43,7 @@ plot(G0vary,mean(temporal2G0,2),"o")
 
 nexttile
 plot(Tvary,mean(temporal3T,2),"o")
+ylabel('Mean(m3)','FontSize',12)
 
 nexttile
 plot(lambdavary,mean(temporal3lambda,2),"o")
@@ -55,6 +59,7 @@ plot(G0vary,mean(temporal3G0,2),"o")
 
 nexttile
 plot(Tvary,var(temporal0T'),"o")
+ylabel('Var(m0)','FontSize',12)
 
 nexttile
 plot(lambdavary,var(temporal0lambda'),"o")
@@ -67,18 +72,20 @@ plot(G0vary,var(temporal0G0'),"o")
 
 nexttile
 plot(Tvary,var(temporal1T'),"o")
+ylabel('Var(m1)','FontSize',12)
 
 nexttile
 plot(lambdavary,var(temporal1lambda'),"o")
 
 nexttile
-plot(sigma_noise,var(temporal1sigma_noise'),"o")
+plot(sigma_noise_vary,var(temporal1sigma_noise'),"o")
 
 nexttile
 plot(G0vary,var(temporal1G0'),"o")
 
 nexttile
 plot(Tvary,var(temporal2T'),"o")
+ylabel('Var(m2)','FontSize',12)
 
 nexttile
 plot(lambdavary,var(temporal2lambda'),"o")
@@ -92,14 +99,18 @@ plot(G0vary,var(temporal2G0'),"o")
 
 nexttile
 plot(Tvary,var(temporal3T'),"o")
+xlabel("T",'FontSize',16)
+ylabel('Var(m3)','FontSize',12)
 
 nexttile
 plot(lambdavary,var(temporal3lambda'),"o")
+xlabel("\lambda",'FontSize',16)
 
 nexttile
 plot(sigma_noise_vary,var(temporal3sigma_noise'),"o")
+xlabel("\sigma_N",'FontSize',16)
 
 nexttile
 plot(G0vary,var(temporal3G0'),"o")
-
+xlabel("G0",'FontSize',16)
 
--- Matlab/statistics/statistics.m
+++ Matlab/statistics/statistics.m
@@ -13,13 +13,13 @@
 clear all
 
 %T = linspace(7.8e-11,7.8e-9,50); % Reverberation time, 
-a = 7.8e-11; b = 7.8e-7,Nl = 50;
+a = 7.8e-11; b = 7.8e-7,Nl = 200;
 Tvary = a + (b-a).*rand(Nl,1);
 G0 = db2pow(-83.9); % Reverberation gain converted from dB to power
 
 % Time delay tau is a possion arrival process with mean delay lambda
 lambda = 10e8; % randomly chosen arrival rate lambda 10e9 arrivals per second
-n = 20; % Number of iterations 
+n = 200; % Number of iterations 
 
 sigma_noise = sqrt(0.28e-9); % noise variance
 for k = 1:length(Tvary)
@@ -80,11 +80,11 @@ title("Var (m3)")
 
 %T = linspace(7.8e-11,7.8e-9,50); % Reverberation time, 
 T = 7.8e-9;
-a = db2pow(-110); b = db2pow(-60),Nl = 50;
+a = db2pow(-110); b = db2pow(-60),Nl = 200;
 G0vary = a + (b-a).*rand(Nl,1);
 % Time delay tau is a possion arrival process with mean delay lambda
 lambda = 10e8; % randomly chosen arrival rate lambda 10e9 arrivals per second
-n = 20; % Number of iterations 
+n = 200; % Number of iterations 
 
 sigma_noise = sqrt(0.28e-9); % noise variance
 for k = 1:length(G0vary)
@@ -148,11 +148,11 @@ title("Var (m3)")
 %T = linspace(7.8e-11,7.8e-9,50); % Reverberation time, 
 T = 7.8e-9;
 G0 = db2pow(-83.9);
-a = sqrt(5e-10); b = sqrt(5e-9),Nl = 50;
+a = sqrt(5e-10); b = sqrt(5e-9),Nl = 200;
 sigma_noise_vary = a + (b-a).*rand(Nl,1);
 % Time delay tau is a possion arrival process with mean delay lambda
 lambda = 10e8; % randomly chosen arrival rate lambda 10e9 arrivals per second
-n = 20; % Number of iterations 
+n = 200; % Number of iterations 
 
 for k = 1:length(sigma_noise_vary)
     [P Pv alpha tau, t] = turin_sim_alpha_cartesian_form_claus(lambda,G0,T,n,sigma_noise_vary(k));
@@ -215,10 +215,10 @@ title("Var (m3)")
 %T = linspace(7.8e-11,7.8e-9,50); % Reverberation time, 
 T = 7.8e-9;
 G0 = db2pow(-83.9);
-a = 50e6; b = 50e7,Nl = 50;
+a = 50e6; b = 50e7,Nl = 200;
 lambdavary = a + (b-a).*rand(Nl,1)
 % Time delay tau is a possion arrival process with mean delay lambda
-n = 20; % Number of iterations 
+n = 200; % Number of iterations 
 
 sigma_noise = sqrt(0.28e-9); % noise variance
 for k = 1:length(lambdavary)

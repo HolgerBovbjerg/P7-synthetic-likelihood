7ecc8da6498a5447fc4057ae5ef718577fe68685
Peter_Fisker <pet5631@gmail.com>
2020-10-16 15:39:17 +0200

    Added tiled plot for histograms

--- Matlab/statistics/summary_statistics_distributions.m
+++ Matlab/statistics/summary_statistics_distributions.m
@@ -3,22 +3,28 @@ clear
 T = 7.8e-9; % Reverberation time: 7.8 ns
 G0 = db2pow(-83.9); % Reverberation gain converted from dB to power
 sigma_N = sqrt(28e-9); % Noise variance
-N = 10; 
+N = 200000; % number of realizations drawn from the turin model
 B = 4e9; % Bandwidth of signal: 4 GHz
-Ns = 801;
+Ns = 801; % Number of frequency samples in transfer function
+
 % Time delay tau is a possion arrival process with mean delay lambda
 lambda = 10e9; % randomly chosen arrival rate lambda 10e9 arrivals per second
-Nr = 10;
+
+Nr = 1;
 S = zeros(8,Nr);
-t_per_run = 0.005;
-est_time = N*Nr*t_per_run;
+tic
+[q, w] = sim_turin_matrix_gpu(10, B, Ns, T, G0, lambda, sigma_N);
+t_per_run = toc/10;
+
+
+est_time = N*Nr*t_per_run
 
 %%
 temporal0 = gpuArray(zeros(N,Nr));
 temporal1 = gpuArray(zeros(N,Nr));
 temporal2 = gpuArray(zeros(N,Nr));
 temporal3 = gpuArray(zeros(N,Nr));
-
+tic
 for k = 1:Nr
     
     [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, T, G0, lambda, sigma_N);
@@ -38,3 +44,41 @@ for k = 1:Nr
         var(temporal2(:,k)); ...
         var(temporal3(:,k)) ];      
 end
+toc
+%%
+bins = 11;
+figure(1)
+plotstatistics =  tiledlayout(2,4);
+title(plotstatistics,"Histograms of summary statistics, from "+ Nr +" summary statistic vectors") 
+
+nexttile
+hist(S(1,:),bins)
+title("Histogram of mean of 0th temporal moment")
+
+nexttile
+hist(S(2,:),bins)
+title("Histogram of mean of 1st temporal moment")
+
+nexttile
+hist(S(3,:),bins)
+title("Histogram of mean of 2nd temporal moment")
+
+nexttile
+hist(S(4,:),bins)
+title("Histogram of mean of 3rd temporal moment")
+
+nexttile
+hist(S(5,:),bins)
+title("Histogram of variance of 0th temporal moment")
+
+nexttile
+hist(S(6,:),bins)
+title("Histogram of variance of 1st temporal moment")
+
+nexttile
+hist(S(7,:),bins)
+title("Histogram of variance of 2nd temporal moment")
+
+nexttile
+hist(S(8,:),bins)
+title("Histogram of variance of 3rd temporal moment")

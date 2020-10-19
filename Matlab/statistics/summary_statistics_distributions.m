74dd46c2aeb2c4fe4cca07b9e048cee97b906fce
Peter_Fisker <pet5631@gmail.com>
2020-10-19 16:23:25 +0200

    Updated script to use varying parameters for each summary statistic

--- Matlab/statistics/summary_statistics_distributions.m
+++ Matlab/statistics/summary_statistics_distributions.m
@@ -9,37 +9,60 @@ Ns = 801; % Number of frequency samples in transfer function
 % Time delay tau is a possion arrival process with mean delay lambda
 lambda = 10e9; % randomly chosen arrival rate lambda 10e9 arrivals per second
 
-Nr = 1000;
-S = zeros(4,Nr);
+Nr = 50;    % Number of Turin realizations pr summary statistic vector
+Nl = 1000;   % Number of different summary statistic vectors generated
+
+ss = zeros(Nl,8); % Matrix for summary statistics
+
 tic
-temp = create_statistics(10, T, G0, lambda, sigma_N, B, Ns);
-t_per_run = toc/10;
+temp = create_statistics(Nr, T, G0, lambda, sigma_N, B, Ns);
+t_per_stat = toc/Nr;
 
 
-est_time = Nr*t_per_run
+est_time = Nl*t_per_stat
+
+%%
+
+T_min = 7.8e-11; T_max = 7.8e-7;
+T = T_min + (T_max-T_min).*rand(Nl,1);
+
+G0_min = db2pow(-110); G0_max = db2pow(-60);
+G0 = G0_min + (G0_max-G0_min).*rand(Nl,1);
+
+sigma_N_min = sqrt(5e-10); sigma_N_max = sqrt(5e-9);
+sigma_N = sigma_N_min + (sigma_N_max-sigma_N_min).*rand(Nl,1);
+
+lambda_min = 50e6; lambda_max = 50e7;
+lambda = lambda_min + (lambda_max-lambda_min).*rand(Nl,1);
 
 %%
 tic
-S = create_statistics(Nr, T, G0, lambda, sigma_N, B, Ns);
+for i = 1:Nl    
+    ss(i,:) = create_statistics(Nr, T(i), G0(i), lambda(i), sigma_N(i), B, Ns);
+end
 toc
 %%
-bins = 15;
+bins = 85;
 figure(1)
-plotstatistics =  tiledlayout(1,4);
-title(plotstatistics,"Histograms of summary statistics, from "+ Nr +" summary statistic vectors") 
+plotstatistics =  tiledlayout(1,5);
+title(plotstatistics,"Histograms of summary statistics, from "+ Nl +" summary statistic vectors") 
 
 nexttile
-hist(S(:,1),bins)
+hist((ss(:,1)),bins)
 title("Histogram of mean of 0th temporal moment")
 
 nexttile
-hist(S(:,2),bins)
+hist(ss(:,2),bins)
 title("Histogram of mean of 1st temporal moment")
 
 nexttile
-hist(S(:,3),bins)
+hist(ss(:,3),bins)
 title("Histogram of mean of 2nd temporal moment")
 
 nexttile
-hist(S(:,4),bins)
+hist(ss(:,4),bins)
 title("Histogram of mean of 3rd temporal moment")
+
+nexttile
+hist(ss(:,5),bins)
+title("Histogram of variance of 0th temporal moment")

3275d6feea49811eb65eb472280b0cf7f3d60bad
Peter_Fisker <pet5631@gmail.com>
2020-10-19 09:54:31 +0200

    Updated the function
    changes:
    - Now only takes single parameter values instead of upper and lower bounds
    - Now returns only the summary statistics

--- Matlab/statistics/create_statistics.m
+++ Matlab/statistics/create_statistics.m
@@ -2,26 +2,20 @@
 % upon N realizations from the Turin model. 
 % Each realization is generated from parameters drawn uniformly between the
 % limits given to the function. 
-function [S T G0] = create_statistics(N, T_min, T_max, G0_min, G0_max, lambda_min, lambda_max, sigma_N_min, sigma_N_max)
-
-    T = T_min + (T_max-T_min).*rand(N,1);
-    G0 = G0_min + (G0_max-G0_min).*rand(N,1);
-    lambda = lambda_min + (lambda_max-lambda_min).*rand(N,1);
-    sigma_N = sigma_N_min + (sigma_N_max-sigma_N_min).*rand(N,1);
-    
-    B = 4e9;
-    Ns = 801;
+function S = create_statistics(N, T, G0, lambda, sigma_N, B, Ns)
     
     S = zeros(N,4);
     for i = 1:N
-    
-        [Pv, t] = sim_turin_matrix_gpu(1, B, Ns, T(i), G0(i), lambda(i), sigma_N(i));
+        % Draw a realization from the Turin model based on input parameters
+        [Pv, t] = sim_turin_matrix_gpu(1, B, Ns, T, G0, lambda, sigma_N);
 
-        t = gpuArray(t);
-        S(i,1) = trapz(t,(t.^0.*Pv));
-        S(i,2) = trapz(t,(t.^1.*Pv));
-        S(i,3) = trapz(t,(t.^2.*Pv));
-        S(i,4) = trapz(t,(t.^3.*Pv));
+%         t = gpuArray(t); % Time it and see if it is faster with. 
+
+        % Calculate temporal moments with numerical integration
+        S(i,1) = trapz(t,(Pv));         % 0th moment (t^0*Pv = Pv)
+        S(i,2) = trapz(t,(t.^1.*Pv));   % 1st moment
+        S(i,3) = trapz(t,(t.^2.*Pv));   % 2nd moment
+        S(i,4) = trapz(t,(t.^3.*Pv));   % 3rd moment
 
     end
 end

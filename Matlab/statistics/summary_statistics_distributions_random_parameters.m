f9c5f466b2487e1cbc26f56cb5fa1324cf322e3e
Stationær\Fisker <pfiske15@student.aau.dk>
2020-10-20 21:48:45 +0200

    Major updates to plots
    Changelog:
    - plots are made in for-loops
    - plots are fitted with normal distributions
    - plots show mean and std variation of fitted normal distribution

--- Matlab/statistics/summary_statistics_distributions_random_parameters.m
+++ Matlab/statistics/summary_statistics_distributions_random_parameters.m
@@ -51,35 +51,86 @@ for j = 1:3
     plotstatistics =  tiledlayout(2,4);
     title(plotstatistics,"Histograms of summary statistics, from "+ Nl +" summary statistic vectors, with random input parameters") 
 
-    nexttile
-    histogram(ss1(j,:,1),100)
-    title("Histogram of mean of 0th temporal moment")
-
-    nexttile
-    histogram(ss1(j,:,2),'Normalization','probability')
-    title("Histogram of mean of 1st temporal moment")
-
-    nexttile
-    histogram(ss1(j,:,3),'Normalization','probability')
-    title("Histogram of mean of 2nd temporal moment")
-
-    nexttile
-    histogram(ss1(j,:,4),'Normalization','probability')
-    title("Histogram of mean of 3rd temporal moment")
-
-    nexttile
-    histogram(ss1(j,:,5),'Normalization','probability')
-    title("Histogram of variance of 0th temporal moment")
-
-    nexttile
-    histogram(ss1(j,:,6),'Normalization','probability')
-    title("Histogram of variance of 1st temporal moment")
-
-    nexttile
-    histogram(ss1(j,:,7),'Normalization','probability')
-    title("Histogram of variance of 2nd temporal moment")
-
-    nexttile
-    histogram(ss1(j,:,8),'Normalization','probability')
-    title("Histogram of variance of 3rd temporal moment")
+    for k = 1:4
+        nexttile
+        hold on
+        pd = fitdist(ss1(j,:,k)','Normal');
+        x_vals = linspace(min(ss1(j,:,k)),max(ss1(j,:,k)),bins);
+        mu = mean(pd);
+        sigma = sqrt(var(pd));
+        histogram(ss1(j,:,k),'Normalization','pdf','Binedges',x_vals,'FaceColor','b')
+        p = plot(x_vals,pdf(pd,x_vals),'r');
+        legend(p,"\mu = "+num2str(mu,2)+", \sigma = "+num2str(sigma,2))
+        title("Histogram of mean of temporal moment "+(k-1))
+    end
+    
+    for k = 5:8
+        nexttile
+        hold on
+        pd = fitdist(ss1(j,:,k)','Normal');
+        x_vals = linspace(min(ss1(j,:,k)),max(ss1(j,:,k)),bins);
+        mu = mean(pd);
+        sigma = sqrt(var(pd));
+        histogram(ss1(j,:,k),'Normalization','pdf','Binedges',x_vals,'FaceColor','b')
+        p = plot(x_vals,pdf(pd,x_vals),'r');
+        legend(p,"\mu = "+num2str(mu,2)+", \sigma = "+num2str(sigma,2))
+        title("Histogram of variance of temporal moment "+(k-5))
+    end   
+    
+%     nexttile
+%     hold on
+%     pd = fitdist(ss1(j,:,2)','Normal');
+%     plot(linspace(min(ss1(j,:,2)),max(ss1(j,:,2)),bins),pdf(pd,linspace(min(ss1(j,:,2)),max(ss1(j,:,2)),bins)))
+%     histogram(ss1(j,:,2),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,2)),max(ss1(j,:,2)),bins))
+%     title("Histogram of mean of 1st temporal moment")
+% 
+%     nexttile
+%     hold on
+%     pd = fitdist(ss1(j,:,3)','Normal');
+%     plot(linspace(min(ss1(j,:,3)),max(ss1(j,:,3)),bins),pdf(pd,linspace(min(ss1(j,:,3)),max(ss1(j,:,3)),bins)))
+%     histogram(ss1(j,:,3),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,3)),max(ss1(j,:,3)),bins))
+%     title("Histogram of mean of 2nd temporal moment")
+% 
+%     nexttile
+%     hold on
+%     pd = fitdist(ss1(j,:,4)','Normal');
+%     plot(linspace(min(ss1(j,:,4)),max(ss1(j,:,4)),bins),pdf(pd,linspace(min(ss1(j,:,4)),max(ss1(j,:,4)),bins)))
+%     histogram(ss1(j,:,4),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,4)),max(ss1(j,:,4)),bins))
+%     title("Histogram of mean of 3rd temporal moment")
+% 
+%     nexttile
+%     hold on
+%     pd = fitdist(ss1(j,:,5)','Normal');
+%     plot(linspace(min(ss1(j,:,5)),max(ss1(j,:,5)),bins),pdf(pd,linspace(min(ss1(j,:,5)),max(ss1(j,:,5)),bins)))
+%     histogram(ss1(j,:,5),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,5)),max(ss1(j,:,5)),bins))
+%     title("Histogram of variance of 0th temporal moment")
+% 
+%     nexttile
+%     hold on
+%     pd = fitdist(ss1(j,:,6)','Normal');
+%     plot(linspace(min(ss1(j,:,6)),max(ss1(j,:,6)),bins),pdf(pd,linspace(min(ss1(j,:,6)),max(ss1(j,:,6)),bins)))
+%     histogram(ss1(j,:,6),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,6)),max(ss1(j,:,6)),bins))
+%     title("Histogram of variance of 1st temporal moment")
+% 
+%     nexttile
+%     hold on
+%     pd = fitdist(ss1(j,:,7)','Normal');
+%     plot(linspace(min(ss1(j,:,7)),max(ss1(j,:,7)),bins),pdf(pd,linspace(min(ss1(j,:,7)),max(ss1(j,:,7)),bins)))
+%     histogram(ss1(j,:,7),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,7)),max(ss1(j,:,7)),bins))
+%     title("Histogram of variance of 2nd temporal moment")
+% 
+%     nexttile
+%     hold on
+%     pd = fitdist(ss1(j,:,8)','Normal');
+%     plot(linspace(min(ss1(j,:,8)),max(ss1(j,:,8)),bins),pdf(pd,linspace(min(ss1(j,:,8)),max(ss1(j,:,8)),bins)))
+%     histogram(ss1(j,:,8),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,8)),max(ss1(j,:,8)),bins))
+%     title("Histogram of variance of 3rd temporal moment")
 end
+%%
+pd = fitdist(ss1(j,:,1)','Normal');
+figure(1)
+hold on
+% histogram(ss1(j,:,1),'Binedges',linspace(min(ss1(j,:,1)),max(ss1(j,:,1)),bins))
+histogram(ss1(j,:,1),'Normalization','pdf','Binedges',linspace(min(ss1(j,:,1)),max(ss1(j,:,1)),bins))
+plot(linspace(min(ss1(j,:,1)),max(ss1(j,:,1)),bins),pdf(pd,linspace(min(ss1(j,:,1)),max(ss1(j,:,1)),bins)))
+title("Histogram of mean of 0th temporal moment")

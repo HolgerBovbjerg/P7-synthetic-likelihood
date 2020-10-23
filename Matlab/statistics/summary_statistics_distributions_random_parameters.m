967e5ac873a1d39d1d8744577fc11dc2dd148586
Peter_Fisker <pet5631@gmail.com>
2020-10-23 17:09:46 +0200

    Cleaned a bit in the script.

--- Matlab/statistics/summary_statistics_distributions_random_parameters.m
+++ Matlab/statistics/summary_statistics_distributions_random_parameters.m
@@ -10,9 +10,9 @@ Ns = 801; % Number of frequency samples in transfer function
 lambda = 10e9; % randomly chosen arrival rate lambda 10e9 arrivals per second
 
 Nr = 100;    % Number of Turin realizations pr summary statistic vector
-Nl = 600;   % Number of different summary statistic vectors generated
+Nl = 1500;   % Number of different summary statistic vectors generated
 
-ss1 = zeros(3,Nl,8); % Matrix for summary statistics
+ss1 = zeros(3,Nl,6); % Matrix for summary statistics
 
 tic
 temp = create_statistics(Nr, T, G0, lambda, sigma_N, B, Ns);
@@ -28,7 +28,7 @@ T = T_min + (T_max-T_min).*rand(Nl,1);
 
 G0_min = db2pow(-110); G0_max = db2pow(-60);
 G0 = G0_min + (G0_max-G0_min).*rand(Nl,1);
-G0 = sort(G0);
+
 sigma_N_min = sqrt(5e-10); sigma_N_max = sqrt(5e-9);
 sigma_N = sigma_N_min + (sigma_N_max-sigma_N_min).*rand(Nl,1);
 
@@ -37,7 +37,7 @@ lambda = lambda_min + (lambda_max-lambda_min).*rand(Nl,1);
 
 %%
 tic
-for j = 1:6
+for j = 1:3
     for i = 1:Nl    
         ss1(j,i,:) = create_statistics(Nr, T(j), G0(j), lambda(j), sigma_N(j), B, Ns);
     end
@@ -45,41 +45,40 @@ end
 toc
 %%
 bins = 25;
-
-for j = 1:6
+plotstatistics = zeros(2,1);
+for j = 1:3
     figure(j)
-    plotstatistics(j) =  tiledlayout(2,4);
-    title(plotstatistics,"Histograms of summary statistics, from "+ Nl +" summary statistic vectors, with random input parameters") 
+    plotstatistics(j) =  tiledlayout(2,3);
+    title(plotstatistics(j),"Histograms of logarithm of summary statistics, from "+ Nl +" summary statistic vectors, with random input parameters") 
 
-    for k = 1:4
+    for k = 1:3
         nexttile
         hold on
-        pd_normal = fitdist(ss1(j,:,k)','Normal');
-        pd_lognormal = fitdist(ss1(j,:,k)','Lognormal');
         x_vals = linspace(min(ss1(j,:,k)),max(ss1(j,:,k)),bins);
-        histogram(ss1(j,:,k),'Normalization','pdf','Binedges',x_vals,'FaceColor','b')
-        p_normal = plot(x_vals,pdf(pd_normal,x_vals),'r', 'Linewidth',3);
-        p_lognormal = plot(x_vals,pdf(pd_lognormal,x_vals),'g', 'Linewidth',3);
-        legend([p_normal p_lognormal],{'Normal','Lognormal'})
-%         legend([p_normal p_lognormal],{"\mu = "+num2str(pd_normal.mu,2)+", \sigma = "+num2str(pd_normal.sigma,2),'Lognormal'})
+        pd_lognormal = fitdist(ss1(j,:,k)','Lognormal');   
+        pd_normal = fitdist(ss1(j,:,k)','Normal');
+        histogram(ss1(j,:,k),'Normalization','pdf','Binedges',x_vals,'FaceColor','b')       
+        p_lognormal = plot(x_vals,pdf(pd_lognormal,x_vals),'g', 'Linewidth',2);
+        p_normal = plot(x_vals,pdf(pd_normal,x_vals),'r', 'Linewidth',2);
+        legend([p_lognormal p_normal],{'Lognormal fit', 'Normal fit'});
         title("     Histogram of mean of temporal moment "+(k-1))
     end
     
-    for k = 5:8
+    for k = 4:6
         nexttile
         hold on
-        pd_normal = fitdist(ss1(j,:,k)','Normal');
-        pd_lognormal = fitdist(ss1(j,:,k)','Lognormal');
         x_vals = linspace(min(ss1(j,:,k)),max(ss1(j,:,k)),bins);
+        pd_lognormal = fitdist(ss1(j,:,k)','Lognormal');   
+        pd_normal = fitdist(ss1(j,:,k)','Normal');
         histogram(ss1(j,:,k),'Normalization','pdf','Binedges',x_vals,'FaceColor','b')
-        p_normal = plot(x_vals,pdf(pd_normal,x_vals),'r', 'Linewidth',3);
-        p_lognormal = plot(x_vals,pdf(pd_lognormal,x_vals),'g', 'Linewidth',3);
-        legend([p_normal p_lognormal],{'Normal','Lognormal'})
-%         legend([p_normal p_lognormal],{"\mu = "+num2str(pd_normal.mu,2)+", \sigma = "+num2str(pd_normal.sigma,2),'Lognormal'})
-        title("          Histogram of variance of temporal moment "+(k-5))
+        p_lognormal = plot(x_vals,pdf(pd_lognormal,x_vals),'g', 'Linewidth',2);
+        p_normal = plot(x_vals,pdf(pd_normal,x_vals),'r', 'Linewidth',2);
+        legend([p_lognormal p_normal],{'Lognormal fit', 'Normal fit'});
+        title("          Histogram of variance of temporal moment "+(k-4))
     end   
 end
 %%
-% for j = 1:6
-%     exportgraphics(plotstatistics(j),"Distribution_of_summary_statistics_with_fixed_parameter_set_"+num2str(j)+".pdf",'ContentType','vector')
+% for j = 1:3
+%     exportgraphics(plotstatistics(j),"Distribution_of_logarithm_of_summary_statistics_with_random_parameter_set_"+num2str(j)+".pdf",'ContentType','vector')
 % end
+%   

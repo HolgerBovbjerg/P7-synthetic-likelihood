536990b3e5be500d21bb28783d7481afd8583d67
Peter_Fisker <pet5631@gmail.com>
2020-10-28 13:29:45 +0100

    Removed statistics based on 2nd temporal moment.

--- Matlab/statistics/create_statistics.m
+++ Matlab/statistics/create_statistics.m
@@ -4,7 +4,7 @@
 % limits given to the function. 
 function S = create_statistics(N, T, G0, lambda, sigma_N, B, Ns)
     
-    S = zeros(1,6);
+    S = zeros(1,4);
     
     % Draw a realization from the Turin model based on input parameters
     [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, T, G0, lambda, sigma_N);
@@ -13,16 +13,16 @@ function S = create_statistics(N, T, G0, lambda, sigma_N, B, Ns)
     % Calculate temporal moments with numerical integration
     m0 = trapz(t,(Pv));         % 0th moment (t^0*Pv = Pv)
     m1 = trapz(t,(t.^1.*Pv));   % 1st moment
-    m2 = trapz(t,(t.^2.*Pv));   % 2nd moment
+%     m2 = trapz(t,(t.^2.*Pv));   % 2nd moment
 
 
     % Calculate summary statistics from moments
-    S(1) = mean(m0);            % Mean of 0th moment 
-    S(2) = mean(m1);            % Mean of 1st moment
-    S(3) = mean(m2);            % Mean of 2nd moment
-    S(4) = var(m0);             % Variance of 0th moment
-    S(5) = var(m1);             % Variance of 1st moment
-    S(6) = var(m2);             % Variance of 2nd moment
+    S(1) = log(mean(m0));            % Mean of 0th moment 
+    S(2) = log(mean(m1));            % Mean of 1st moment
+%     S(3) = mean(m2);            % Mean of 2nd moment
+    S(3) = log(var(m0));             % Variance of 0th moment
+    S(4) = log(var(m1));             % Variance of 1st moment
+%     S(6) = var(m2);             % Variance of 2nd moment
 
 
 end

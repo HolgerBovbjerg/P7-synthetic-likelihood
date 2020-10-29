b3337d8498a466c14557d7718cac64fb0d3fe868
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2020-10-29 22:36:28 +0100

    Update ABC script and streamline code with

--- Matlab/ABC Algorithms/ABC_REJ.m
+++ Matlab/ABC Algorithms/ABC_REJ.m
@@ -1,164 +1,112 @@
-
 %% ABC implementation 2 - ABC REJECTION ALGORITHM:
-
+clear
 %% --- Global turin model simulation parameters ---------------------------------------------------
 N  = 50;    % Number of different turin simulations.
-Ns = 600;   % Number of time entries for each turin simulation. 
+Ns = 801;   % Number of time entries for each turin simulation. 
 Bw = 4e9;   % Bandwidth (4Ghz).
 
 %% --- Generate "observed data" -----------------------------------------------------
-param_T       = 7.8e-9; 
-param_G0      = 4.07e-9;    % dB = -83.9  
-param_lambda  = 10e9;       % arrival rate (1/s)    
-sigma_N       = 1.673e-4;   % equal to sqrt(28e-9)
+param_T_obs       = 7.8e-9;  % Reverberation time
+param_G0_obs      = db2pow(-83.9);    % linear gain 
+param_lambda_obs  = 10e9;       % arrival rate (1/s)    
+sigma_N_obs       = sqrt(0.28e-9);   % Noise std
+M = 200; %number of summary statisctics realisations
 
-[P_Y_observed, t_observed] = sim_turin_matrix(N, Bw, Ns, param_T, param_G0, param_lambda, sigma_N);
-
-% Do summary statistics on the observed data 
-S_observed = sumStatMeanMoment(t_observed, P_Y_observed); 
-disp('Summary statistics of observed data generated...')
+% S_obs = create_statistics(M, N, param_T_obs , param_G0_obs,...
+% param_lambda_obs, sigma_N_obs, Bw, Ns); % create new summary statistic of
+% observed data
+load S_obs;
+%%
+mu_S_obs = mean(S_obs);
+Sigma_S_obs = cov(S_obs);
 
 %% --- Initial max/min conditions for parameters (prior distribution) -----------------------------
 % a = min , b = max
- T_a = 7.8e-11; 
- T_b = 7.8e-7;  
- G0_a = 8.89e-11;    % Power gain (not in dB)
- G0_b = 4.07e-7;     % Power gain (not in dB)
- lambda_a = 1e8;
- lambda_b = 20e10;
- sigmaN_a = 1.673e-6; 
- sigmaN_b = 1.673e-2;
+ T_a = 15e-9; 
+ T_b = 1e-9;  
+ G0_a = db2pow(-70);    % Power gain (not in dB)
+ G0_b = db2pow(-90);     % Power gain (not in dB)
+ lambda_a = 20e8;
+ lambda_b = 20e9;
+ sigmaN_a = sqrt(0.5e-9); 
+ sigmaN_b = sqrt(0.1e-9);
  
 %% --- ABC rejection algorithm ---------------------------------------------------------------------
-% Rejection ratio = 1 - (nbr_extract / sumstat_iter) 
-
 % Set total iterations
-iterations = 100;
+iterations = 3;
 
 % Number of summary statistics sets to generate  
-sumstat_iter = 200;
+sumstat_iter = 2000;
 
 % Extract this amount of parameter entries from each generated summary
 % statistic
-nbr_extract = 20;
+nbr_extract = 100;
 
 % Preallocation of vectors: 
 out = zeros(5,sumstat_iter);
 meanVar_params = zeros(8,iterations);
 
-index = 1; % used as index for vector holding ALL extracted parameter values 
+params_T = zeros(iterations,nbr_extract);
+params_G0 = zeros(iterations,nbr_extract);
+params_lambda = zeros(iterations,nbr_extract);
+params_sigma_N = zeros(iterations,nbr_extract);
 
 disp('ABC algorithm computing, please wait... ')
 tic
 for a = 1:iterations 
+    out = zeros(5,sumstat_iter);
+    d = zeros(sumstat_iter,1);
     for i = 1:sumstat_iter
-        %% STEP 1: Sample parameter from predefined prior distribution (uniform):
-        
+        %% STEP 1: Sample parameter from predefined prior distribution (uniform):      
         % T (Reverberation time):
-        param_T = T_a + (T_b-T_a).*rand(1,1); % generate one random number
+        param_T = T_a + (T_b-T_a)*rand; % generate one random number
         % G0 (Reverberation gain)  
-        param_G0 = (G0_a + (G0_b-G0_a).*rand(1,1)); % generate one random number within the given limits.
+        param_G0 = (G0_a + (G0_b-G0_a)*rand); % generate one random number within the given limits.
         % lambda ()  
-        param_lambda = lambda_a + (lambda_b-lambda_a).*rand(1,1); % generate one random number within the given limits.
+        param_lambda = lambda_a + (lambda_b-lambda_a)*rand; % generate one random number within the given limits.
         % sigma_N (Variance noise floor)
-        param_sigma_N = sigmaN_a + (sigmaN_b-sigmaN_a).*rand(1,1); % generate one random number within the given limits.
+        param_sigma_N = sigmaN_a + (sigmaN_b-sigmaN_a)*rand; % generate one random number within the given limits.
+       
+        %% STEP 2: Simulate data using Turing model, based on parameters from STEP 1 and create statistics
+        S_simulated = create_statistics(1, N, param_T , param_G0, param_lambda, param_sigma_N, Bw, Ns);
+        %% STEP 3: calculate the difference between observed and simulated summary statistics 
+        % Mahalanobis distance see formular in document.
+        d(i) = (S_simulated - mu_S_obs)/Sigma_S_obs * (S_simulated - mu_S_obs)';
         
-        %% STEP 2: Simulate data using Turing model, based on parameters from STEP 1 
-        [P_Y, t] = sim_turin_matrix(N, Bw, Ns, param_T, param_G0, param_lambda, param_sigma_N);
-        
-        %% STEP 3: Do summary statistics on the simulated dataset: 
-        S_simulated = sumStatMeanMoment(t, P_Y);
-         
-        %% STEP 4: calculate the difference between observed and simulated summary statistics 
-        % Euclidean distance see formular in document.
-        SS1 = ((S_simulated(1) - S_observed(1))/S_simulated(4))^2;
-        SS2 = ((S_simulated(2) - S_observed(2))/S_simulated(5))^2;
-        SS3 = ((S_simulated(3) - S_observed(3))/S_simulated(6))^2;
-        
-        % Row 1 of the out vector contains the euclidean distance 
+        % Row 1 of the out vector contains the distance 
         % the rest of the rows contains the corresponding parameters 
         % used for generating that specific distance.    
-        out(1,i) =  ((SS1 + SS2 + SS3)^(0.5));         
+        out(1,i) =  d(i);         
         out(2,i) =  param_T;
         out(3,i) =  param_G0;
         out(4,i) =  param_lambda;
         out(5,i) =  param_sigma_N;
-        
         disp(i);
-        
     end 
-    
-    % Transpose the out vector in order to use the function "sortrow"
-    out = out';
     % Sort the "out" matrix so that the lowest euclidean distance is at the
     % (1,1) matrix position and highest distance is at (max,1) 
-    out = sortrows(out); 
-    % Transpose matrix back 
-    out = out';
-    
-    % Preallocating vectors
-    params_T_lastExtract       = zeros(1,nbr_extract);
-    params_G0_lastExtract      = zeros(1,nbr_extract);
-    params_lambda_lastExtract  = zeros(1,nbr_extract);
-    params_sigma_N_lastExtract = zeros(1,nbr_extract);
-    
-    params_T       = zeros(1,nbr_extract*iterations);
-    params_G0      = zeros(1,nbr_extract*iterations);
-    params_lambda  = zeros(1,nbr_extract*iterations);
-    params_sigma_N = zeros(1,nbr_extract*iterations);
-    
-    % Extract the parameters that was used for generating the summary
-    % statistics that was closest to the observed summary statistics
-    % i.e had the lowest euclidean distances
-   
-    for i = 1:nbr_extract
-        % Following vectors holds the CURRENT extracted parameter values 
-        % that was within euclidean distance       
-        params_T_lastExtract(i)       = out(2,i);
-        params_G0_lastExtract(i)      = out(3,i);
-        params_lambda_lastExtract(i)  = out(4,i);
-        params_sigma_N_lastExtract(i) = out(5,i);
-        
-        % Following vectors holds ALL extracted parameter values 
-        % that was within euclidean distance
-        params_T(index)       = out(2,i);
-        params_G0(index)      = out(3,i);
-        params_lambda(index)  = out(4,i);
-        params_sigma_N(index) = out(5,i);   
-        
-        index = index + 1; 
-    end
+    out = sortrows(out',1)';
+
+    % Following vectors holds ALL extracted parameter values 
+    % that was within euclidean distance
+    params_T(a,:)       = out(2,1:nbr_extract);
+    params_G0(a,:)      = out(3,1:nbr_extract);
+    params_lambda(a,:)  = out(4,1:nbr_extract);
+    params_sigma_N(a,:) = out(5,1:nbr_extract);
+
     
    % Update the min/max values for the parameters for the next iteration 
-   T_a      = min(params_T_lastExtract);
-   T_b      = max(params_T_lastExtract);
-   G0_a     = min(params_G0_lastExtract);
-   G0_b     = max(params_G0_lastExtract);
-   lambda_a = min(params_lambda_lastExtract);
-   lambda_b = max(params_lambda_lastExtract);
-   sigmaN_a = min(params_sigma_N_lastExtract);
-   sigmaN_b = max(params_sigma_N_lastExtract);
-   
-   % Calculate the mean and variance for each parameter from 
-   % each iteration, and save in the matrix meanVar_params. 
-   % Column 1 contains all the means and variances 
-   % from iteration 1, column 2 for iteration 2...
-   meanVar_params(1,a) = mean(params_T_lastExtract);
-   meanVar_params(2,a) = mean(params_G0_lastExtract);
-   meanVar_params(3,a) = mean(params_lambda_lastExtract);
-   meanVar_params(4,a) = mean(params_sigma_N_lastExtract);
-   meanVar_params(5,a) = var(params_T_lastExtract);
-   meanVar_params(6,a) = var(params_G0_lastExtract);
-   meanVar_params(7,a) = var(params_lambda_lastExtract);
-   meanVar_params(8,a) = var(params_sigma_N_lastExtract);
-   % The mean and variances can now be used to plot a normal distribution of the parameters
-   % for all iterations.
-   
+   T_a      = min(params_T(a,:));
+   T_b      = max(params_T(a,:));
+   G0_a     = min(params_G0(a,:));
+   G0_b     = max(params_G0(a,:));
+   lambda_a = min(params_lambda(a,:));
+   lambda_b = max(params_lambda(a,:));
+   sigmaN_a = min(params_sigma_N(a,:));
+   sigmaN_b = max(params_sigma_N(a,:));
    disp(a);
 end 
-
 toc
-
    
 %{    
 t = tiledlayout(2,2, 'TileSpacing', 'none', 'Padding', 'compact');

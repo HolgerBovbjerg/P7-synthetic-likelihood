29321e6f6870aabc7d9a8398da3e78e52a97079c
Stationær\Fisker <pfiske15@student.aau.dk>
2020-11-04 17:18:05 +0100

    Fixed indentations

--- BSL_v2/main.m
+++ BSL_v2/main.m
@@ -1,6 +1,4 @@
 clear all
-
-
 N = 200; % Number of Turin simulations
 
 T_true = 7.8e-9;
@@ -30,77 +28,76 @@ end
 loglikelihood = synth_loglikelihood(s_obs,s_sim)
 
 accept = 0;
-h = 1200;
-k = 1000; % Number of MCMC steps
-thetas= zeros(4,h+k);
+h = 1200;  % Number of MCMC steps
+k = 100;
+thetas = zeros(4,h+k);
 likelihoods = zeros(1,N);
 thetas(:,1) = theta_curr';
  
-  for j = 2:k
-      j
-      L = 10; % Numberof statistics vectors used per likelihood
-      theta_prop = mvnrnd(theta_curr,covariance);
-      while(any(theta_prop < 0))
-        theta_prop = mvnrnd(theta_curr,covariance);
-      end
+for j = 2:k
+    j
+    theta_prop = mvnrnd(theta_curr,covariance);
+    while(any(theta_prop < 0))
+    theta_prop = mvnrnd(theta_curr,covariance);
+    end
 
-      parfor i = 1:L
-          [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, theta_prop(1), theta_prop(2), theta_prop(3), theta_prop(4));
-          s_sim(i,:) = create_statistics(Pv, t);
-      end
-      loglikelihoodnew = (synth_loglikelihood(s_obs,s_sim));
-    
-      likeli = exp(loglikelihoodnew-loglikelihood);
-      if likeli >  rand 
-        loglikelihood = loglikelihoodnew;
-        theta_curr = theta_prop;
-        accept = accept+1
-      end
-      thetas(:,j) = theta_curr';
-  end   
-  
+    parfor i = 1:L
+      [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, theta_prop(1), theta_prop(2), theta_prop(3), theta_prop(4));
+      s_sim(i,:) = create_statistics(Pv, t);
+    end
+    loglikelihoodnew = (synth_loglikelihood(s_obs,s_sim));
+
+    likeli = exp(loglikelihoodnew-loglikelihood);
+    if likeli >  rand 
+    loglikelihood = loglikelihoodnew;
+    theta_curr = theta_prop;
+    accept = accept+1
+    end
+    thetas(:,j) = theta_curr';
+end   
+
 
 %%
 
-  for j = k+1:h+k
-      j
-      covariance = adaptive_cov(thetas(:,1:j-1),covariance,j-1);
-      theta_prop = mvnrnd(theta_curr,covariance);
-      while(any(theta_prop < 0))
-        theta_prop = mvnrnd(theta_curr,covariance);
-      end
-      parfor i = 1:L
-          [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, theta_prop(1), theta_prop(2), theta_prop(3), theta_prop(4));
-          s_sim(i,:) = create_statistics(Pv, t);
-      end
-      loglikelihoodnew = (synth_loglikelihood(s_obs,s_sim));
-      
-      likeli = exp(loglikelihoodnew-loglikelihood);
-      if likeli >  rand
-          loglikelihood = loglikelihoodnew;
-          theta_curr = theta_prop;
-          accept = accept+1
-      end
-      thetas(:,j) = theta_curr';
-  end
+for j = k+1:h+k
+    j
+    covariance = adaptive_cov(thetas(:,1:j-1),covariance,j-1);
+    theta_prop = mvnrnd(theta_curr,covariance);
+    while(any(theta_prop < 0))
+    theta_prop = mvnrnd(theta_curr,covariance);
+    end
+    parfor i = 1:L
+      [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, theta_prop(1), theta_prop(2), theta_prop(3), theta_prop(4));
+      s_sim(i,:) = create_statistics(Pv, t);
+    end
+    loglikelihoodnew = (synth_loglikelihood(s_obs,s_sim));
+
+    likeli = exp(loglikelihoodnew-loglikelihood);
+    if likeli >  rand
+      loglikelihood = loglikelihoodnew;
+      theta_curr = theta_prop;
+      accept = accept+1
+    end
+    thetas(:,j) = theta_curr';
+end
   %%
-  tiledlayout(4,1)
-  nexttile
-  plot(thetas(1,:),'o')
-  yline(T_true)
-  title("T")
-  
-    nexttile
-  plot(thetas(2,:),'o')
-  yline(G0_true)
-  title("G0")
-  
-    nexttile
-  plot(thetas(3,:),'o')
-  yline(lambda_true)
-  title("\lambda")
-  
-    nexttile
-  plot(thetas(4,:),'o')
-  yline(sigma_N_true)
-  title("\sigma")
+tiledlayout(4,1)
+nexttile
+plot(thetas(1,:),'o')
+yline(T_true)
+title("T")
+
+nexttile
+plot(thetas(2,:),'o')
+yline(G0_true)
+title("G0")
+
+nexttile
+plot(thetas(3,:),'o')
+yline(lambda_true)
+title("\lambda")
+
+nexttile
+plot(thetas(4,:),'o')
+yline(sigma_N_true)
+title("\sigma")

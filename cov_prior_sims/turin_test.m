7cb6ee065edf11275efd2df1cddc48542ec83483
clausmeyer <32387509+clausmeyer@users.noreply.github.com>
2020-11-10 15:36:28 +0100

    added turin test data

--- cov_prior_sims/turin_test.m
+++ cov_prior_sims/turin_test.m
@@ -1,41 +1,57 @@
 clear all
 
-N = 200; % Number of Turin simulations
+% Data are randomly drawn from the prior range of
+% "Prior_data_small_prior_min_max_values.mat"
+load("thetas_for_turin_tests.mat")
 
-T_true = 7.8e-9;
-G0_true = db2pow(-83.9); % Reverberation gain converted from dB to power - 4.0738e-9
-lambda_true = 4e8;
-sigma_N_true = sqrt(0.28e-9); % noise variance
-
-theta_true = [T_true G0_true lambda_true sigma_N_true];
 Ns = 801; % Number of sample points per Turin simulation
 B = 4e9; % Bandwidth of signal: 4 GHz
 
-sims = 1000;
+sims = 2000;
 
 PV_temp = zeros(801,sims);
 s_obs = zeros(4,sims);
-% "Observed data for testing"
+
 for i = 1:sims
     i
-    [Pv, t] = sim_turin_matrix_gpu(1, B, Ns, theta_true);
+    [Pv, t] = sim_turin_matrix_gpu(1, B, Ns, turin_thetas_1);
     PV_temp(:,i) = Pv;
     s_obs(:,i) = create_statistics(PV_temp(:,1:i), t);
 end
 %%
+load("test_using_turin_thetas_1.mat")
+data1 = s_obs;
+
+load("test_using_turin_thetas_2.mat")
+data2 = s_obs;
+
+load("test_using_turin_thetas_3.mat")
+data3 = s_obs;
+
+load("test_using_turin_thetas_4.mat")
+data4 = s_obs;
+
+ss_obs = [data1; data2; data3; data4];
+%%
+figure(5)
+hold on
 tiledlayout(4,1)
-for q = 1:4
-   nexttile
-   plot(1:i,s_obs(q,:),'Linewidth',2)
-   if q == 1
-       title('$\bar{m}_0$','Interpreter','Latex')
-   elseif q == 2
-       title('$\bar{m}_1$','Interpreter','Latex')
-      elseif q == 3
-       title('$Var(m_0)$','Interpreter','Latex')
-          elseif q == 4
-       title('$Var(m_1)$','Interpreter','Latex')
-   end
+for p = 1:4
+    nexttile
+    hold on
+    for q = 1:4
+        
+        plot(1:1000,ss_obs(4*(q-1)+p,1:1000),'Linewidth',2)
+        if q == 1
+            title('$\bar{m}_0$','Interpreter','Latex')
+        elseif q == 2
+            title('$\bar{m}_1$','Interpreter','Latex')
+        elseif q == 3
+            title('$Var(m_0)$','Interpreter','Latex')
+        elseif q == 4
+            title('$Var(m_1)$','Interpreter','Latex')
+        end
+    end
 end
 %%
 [covariance theta_curr] = find_cov_prior(prior);

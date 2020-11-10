670421d4ec7490be1236d39e7170c8e28e53aa8e
Peter_Fisker <pet5631@gmail.com>
2020-11-10 14:56:35 +0100

    Updated some whoopsies

--- cov_prior_sims/main.m
+++ cov_prior_sims/main.m
@@ -1,6 +1,6 @@
 %%
 clear all
-load('Prior_data_large_prior_min_max_values.mat')
+load('Prior_data_very_small_prior_min_max_values.mat')
 load('Theta_true_values.mat')
 
 N = 50; % Number of Turin simulations
@@ -8,7 +8,7 @@ Ns = 801; % Number of sample points per Turin simulation
 B = 4e9; % Bandwidth of signal: 4 GHz
 %%
 [covariance, theta_curr] = find_cov_prior(prior);
-
+covariance = covariance/100;
 theta_start = theta_curr;
 %%
 % "Observed data for testing"
@@ -17,10 +17,10 @@ load('S_obs_true.mat')
 % [Pv, t] = sim_turin_matrix_gpu(10000, B, Ns, theta_true);
 % s_obs = create_statistics(Pv, t);
 %%
-s_sim = zeros(L,8);
-k = 500;    % Number of MCMC steps
 L = 10;     % Numberof statistics vectors used per likelihood
+k = 500;    % Number of MCMC steps
 accept = 0;
+s_sim = zeros(L,4);
 thetas = zeros(4,k);
 thetas(:,1) = theta_curr';
 

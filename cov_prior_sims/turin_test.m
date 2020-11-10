00933d7a51f75096b6ad3e842f778ae75dcea43c
clausmeyer <32387509+clausmeyer@users.noreply.github.com>
2020-11-10 14:52:45 +0100

    .

--- cov_prior_sims/turin_test.m
+++ cov_prior_sims/turin_test.m
@@ -11,19 +11,31 @@ theta_true = [T_true G0_true lambda_true sigma_N_true];
 Ns = 801; % Number of sample points per Turin simulation
 B = 4e9; % Bandwidth of signal: 4 GHz
 
-PV_temp = zeros(801,10000);
+sims = 1000;
+
+PV_temp = zeros(801,sims);
+s_obs = zeros(4,sims);
 % "Observed data for testing"
-for i = 1:10000
+for i = 1:sims
     i
     [Pv, t] = sim_turin_matrix_gpu(1, B, Ns, theta_true);
     PV_temp(:,i) = Pv;
     s_obs(:,i) = create_statistics(PV_temp(:,1:i), t);
 end
 %%
-tiledlayout(8,1)
-for q = 1:8
+tiledlayout(4,1)
+for q = 1:4
    nexttile
-   plot(1:i-1,s_obs(q,:))
+   plot(1:i,s_obs(q,:),'Linewidth',2)
+   if q == 1
+       title('$\bar{m}_0$','Interpreter','Latex')
+   elseif q == 2
+       title('$\bar{m}_1$','Interpreter','Latex')
+      elseif q == 3
+       title('$Var(m_0)$','Interpreter','Latex')
+          elseif q == 4
+       title('$Var(m_1)$','Interpreter','Latex')
+   end
 end
 %%
 [covariance theta_curr] = find_cov_prior(prior);

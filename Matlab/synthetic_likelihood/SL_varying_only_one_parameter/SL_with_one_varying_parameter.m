34ec8da4665ca758173d800a1cc682c0cc62c084
Peter_Fisker <pet5631@gmail.com>
2020-11-12 15:18:42 +0100

    More data and plots in script

--- Matlab/synthetic_likelihood/SL_varying_only_one_parameter/SL_with_one_varying_parameter.m
+++ Matlab/synthetic_likelihood/SL_varying_only_one_parameter/SL_with_one_varying_parameter.m
@@ -7,7 +7,7 @@ load('Stats_obs_true.mat')
 N = 200; % Number of Turin simulations
 Ns = 801; % Number of sample points per Turin simulation
 B = 4e9; % Bandwidth of signal: 4 GHz
-L = 100;
+L = 50;
 
 %%
 steps = 700;
@@ -20,12 +20,12 @@ loglikelihood = zeros(steps,1);
 
 tic
 for j = 1:steps
-    %     lambda = lambdas(j);
-    %         T = Ts(j);
+%     lambda = lambdas(j);
+%     T = Ts(j);
     sigma = sigmas(j);
-    %     G0 = G0s(j)
+%     G0 = G0s(j)
     parfor i = 1:L
-        theta_prop = [theta_true(1) theta_true(2) theta_true(4) theta_true(4)];
+        theta_prop = [theta_true(1) theta_true(2) theta_true(3) sigma];
 %         theta_prop = [theta_true(1) theta_true(2) lambda theta_true(4)];
 %         theta_prop = [T theta_true(2) theta_true(3) theta_true(4)];
         [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, theta_prop);
@@ -39,18 +39,18 @@ time = toc
 %%
 figure(1)
 hold on
-plot(G0s,exp(loglikelihood_G0_L10_N200),'g')
-plot(G0s,exp(loglikelihood_G0_L50_N200),'m')
-plot(G0s,exp(loglikelihood_G0_L100_N200),'k')
+plot(pow2db(G0s),(loglikelihood_G0_L10_N200),'g')
+plot(pow2db(G0s),(loglikelihood_G0_L50_N200),'m')
+plot(pow2db(G0s),(loglikelihood_G0_L100_N200),'k')
 legend('G0\_L10\_N200','G0\_L50\_N200','G0\_L100\_N200')
-ylim([0 16e5])
-% xline(theta_true(2),'r')
+% ylim([0 16e5])
+xline(pow2db(theta_true(2)),'r')
 %%
 figure(2)
 hold on
-plot(G0s,exp(loglikelihood_G0_L50_N200),'g')
-plot(G0s,exp(loglikelihood_G0_L50_N100),'m')
-plot(G0s,exp(loglikelihood_G0_L50_N50),'k')
+plot(pow2db(G0s),exp(loglikelihood_G0_L50_N200),'g')
+plot(pow2db(G0s),exp(loglikelihood_G0_L50_N100),'m')
+plot(pow2db(G0s),exp(loglikelihood_G0_L50_N50),'k')
 legend('G0\_L50\_N200','G0\_L50\_N100','G0\_L50\_N50')
 % xline(theta_true(2),'r')
 %%
@@ -82,10 +82,11 @@ plot(exp(loglikelihood_Sigma_L200_N10),'k')
 legend('sigmaN\_L100\ N10','sigmaN\_L200\ N10')
 %%
 figure(7)
-hold on
+% hold on
 % plot(G0s,exp(loglikelihood_Sigma_L10_N200),'g')
-% plot(G0s,exp(loglikelihood_Sigma_L50_N200),'m')
-plot(G0s,exp(loglikelihood_Sigma_L100_N200),'k')
-legend('Sigma\_L10\_N200','Sigma\_L50\_N200','Sigma\_L100\_N200')
+plot(sigmas,exp(loglikelihood_Sigma_L50_N200),'m')
+figure(8)
+plot(sigmas,exp(loglikelihood_Sigma_L100_N200),'k')
+% legend('Sigma\_L10\_N200','Sigma\_L50\_N200','Sigma\_L100\_N200')
 % ylim([0 16e5])
 % xline(theta_true(2),'r')

78d14ce54c37615a6ecb34118a5eb7e2d9916651
Stationær\Fisker <pfiske15@student.aau.dk>
2020-12-14 16:25:41 +0100

    updated script for Claus to run test.

--- Matlab/synthetic_likelihood/SL_varying_only_one_parameter/SL_with_one_varying_parameter_backup.m
+++ Matlab/synthetic_likelihood/SL_varying_only_one_parameter/SL_with_one_varying_parameter_backup.m
@@ -4,33 +4,30 @@ load('Prior_data_medium_prior_min_max_values.mat')
 load('Theta_true_values.mat')
 
 
-
 N = 300; % Number of Turin simulations
 Ns = 801; % Number of sample points per Turin simulation
 B = 4e9; % Bandwidth of signal: 4 GHz
-L = 100;
+L = 500;
 
 [Pv, t] = sim_turin_matrix_gpu(1000, B, Ns, theta_true);
 s_obs = create_statistics_new(Pv, t);
 
 %%
-steps = 550;
-G0s = linspace(3.8595e-09,4.28e-9,steps);
-Ts = linspace(7.5e-9,8e-9,steps);
-% lambdas = linspace(prior(3,1),prior(3,2),steps);
+steps = 35;
+% G0s = linspace(3.8595e-09,4.28e-9,steps);
+% Ts = linspace(7.5e-9,8e-9,steps);
+lambdas = linspace(prior(3,1),prior(3,2),steps);
 %%
 loglikelihood = zeros(steps,1);
-
-
 tic
 for j = 1:steps
-%     lambda = lambdas(j);
-    %T = Ts(j);
+    lambda = lambdas(j);
+
     tic
-    G0 = G0s(j);
     parfor i = 1:L
-%         theta_prop = [theta_true(1) theta_true(2) lambda theta_true(4)];
-        theta_prop = [theta_true(1) G0 theta_true(3) theta_true(4)];
+        theta_prop = [1.222097614611416e-08 2.677507662312809e-08 lambda 1.951110337740534e-05];
+%         theta_prop = [theta_true(1) G0 theta_true(3) theta_true(4)];
+                
         [Pv, t] = sim_turin_matrix_gpu(N, B, Ns, theta_prop);
         s_sim(i,:) = create_statistics_new(Pv, t);
     end

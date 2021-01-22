2e76ad4ac0b6830ef801a08ab6a9067533dd6756
Holger Severin Bovbjerg <31927268+HolgerBovbjerg@users.noreply.github.com>
2021-01-22 11:21:20 +0100

    Update measured data script

--- Matlab/real_measured_data_plot/Visualize_measured_data.m
+++ Matlab/real_measured_data_plot/Visualize_measured_data.m
@@ -1,14 +1,14 @@
-%%
-clf
+%% Ramoni code
 Deltat = 1/diff([58 62]*1e9);
 Taxis = (0:801-1)*Deltat;
 % load ('HfullNLOS5.mat');
+
 for ii = 1:50
     for jj = 1:50
-        x(ii,jj,:) =ifft(squeeze(HfullNLOS5(ii,jj,:)));
+        x(ii,jj,:) = ifft(squeeze(HfullNLOS5(ii,jj,:)));
     end
 end
- 
+
 for ii=1:2
     for jj = 1:2
         if ii ==1 && jj ==1 
@@ -22,59 +22,46 @@ for ii=1:2
         end
     end
 end
+Py_measured = abs(h_vv).^2;
+Py_measured_ave = mean(Py_measured);
 
 
-ift_hh = ifft(h_hh);
-ift_vv = ifft(h_vv);
-ift_hv = ifft(h_hv);
-ift_vh = ifft(h_vh);
+%% simulate data
+load("Theta_true_values.mat");
+Ns = 801;
+B = 4e9;
+deltaf = B/(Ns-1);
+
+[Py_sim, ~] = sim_turin_matrix_gpu_w_delay(5000, B, Ns, theta_true, 6e-9);
+
+%% create statistics
+t = (0:Ns-1)'./(deltaf*Ns);
+
+s_obs_measured = create_statistics(Py_measured',t);
+s_obs_simulated = create_statistics(Py_sim,t);
+
+%% plot data
 
 
-
-
-%% 
-load('Theta_true_values.mat')
-
-Theta_delay = [theta_true(1)/1.45 theta_true(2)/100e4 10e6 theta_true(4)/30];
-Theta =       [theta_true(1)/1.45 theta_true(2)/10e4 10e6 theta_true(4)/30];
-theta_test = theta_true;
-theta_test(3) = 0.5e9; 
-N = 300; % Number of Turin simulations
-Ns = 801; % Number of sample points per Turin simulation
-B = 4e9; % Bandwidth of signal: 4 GHz
-
-% Turin with delay
-[P_y, t] = sim_turin_matrix_gpu_w_delay(N, B, Ns, Theta_delay, 1e-8);
-
-[P_y2, t] = sim_turin_matrix_gpu(N, B, Ns, Theta);
-
-[P_y3, t] = sim_turin_matrix_gpu(N, B, Ns, theta_test);
-theta_test(3) = 2e9;
-[P_y4, t] = sim_turin_matrix_gpu(N, B, Ns, theta_true);
-[P_y5, t] = sim_turin_matrix_gpu(N, B, Ns, theta_test);
-%%
-
-plot(t, pow2db(mean(P_y3,2)), 'DisplayName','\lambda = 0.5e9')
+figure
+plot(t,pow2db( mean(abs(h_vv).^2) ))
 hold on
-plot(t, pow2db(mean(P_y4,2)), 'DisplayName','\lambda = 1e9')
-plot(t, pow2db(mean(P_y5,2)), 'DisplayName','\lambda = 2e9')
-legend
+plot(t,pow2db( mean(abs(h_vh).^2) ))
+plot(t,pow2db( mean(abs(h_hv).^2) ))
+plot(t,pow2db( mean(abs(h_hh).^2) ))
 
-%%
-t = (0:Ns-1)'./B;
-figure(1) 
-plot(t,pow2db(  abs(mean(ift_vv).^2) ) )
+fig = figure;
+plot(t*1e9,pow2db( mean(abs(h_vv).^2)) ,'LineWidth',2)
+xtickformat('%d ns')
+ylabel('[dB]')
+box off
 
-hold on 
-plot(t, pow2db(mean(P_y,2)))
-% plot(t, pow2db(mean(P_y2,2)))
+figure
+plot(t,pow2db(Py_measured_ave),t,pow2db(mean(Py_sim,2)))
 
-% plot(t,pow2db(  abs(mean(ift_hh).^2) ) )
-% % plot(t,pow2db(  abs(mean(ift_vh).^2) ) ) % Looks funny...
-% plot(t,pow2db(  abs(mean(ift_hv).^2) ) )
 
 %% 
-exportgraphics(gcf,'observed_measurements_vertical_vertical_average_PDP.pdf','ContentType','vector')
+exportgraphics(fig,'observed_measurements_vertical_vertical_average_PDP.pdf','ContentType','vector')
 
 
 
